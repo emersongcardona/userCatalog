b0ee1453380d768a0a87a9dd58df21fe4bf24896
emersongcardona <emersongcardona@gmail.com>
2024-03-04 18:30:22 -0600

    create the basic CRUD for users

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.send('respond with a user');
+});
+
+router.get('/:id',  (req, res) => {
+  const userId = req.params.id;
+  res.send(`Mostrando artículo con el ID: ${userId}`);
+});
+
+router.post('/',  (req, res) => {
+  const data = req.body;
+  res.send('Datos recibidos correctamente');
+});
+
+router.patch('/', (req,res) =>{
+  res.send("aqui actualizaremos datos")
+})
+
+
+router.delete('/:id',  (req, res) => {
+  const userId = req.params.id;
+  res.send(`borraremos el usuario ${userId}`);
+});
+
+
+
+
+module.exports = router;
